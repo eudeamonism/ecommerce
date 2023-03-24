effa5cfa17ea7ec3f900571a44fd3d8d915abfb8
Evan Vargas <evanvargas@gmail.com>
2023-03-24 15:50:55 -0500

    Initialize Repository

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+	return (
+		<div>
+			<h1 style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>Hello</h1>
+		</div>
+	);
+}
+
+export default App;
