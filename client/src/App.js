047aa6f32dd06a0dfe66648ae5a321a04d18f4ec
Evan Vargas <evanvargas@gmail.com>
2023-04-08 13:27:30 -0500

    Login & Registration

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import ProductScreen from './screens/ProductScreen';
 import Footer from './components/Footer';
 import LandingScreen from './screens/LandingScreen';
 import LoginScreen from './screens/LoginScreen';
+import RegistrationScreen from './screens/RegistrationScreen';
 
 function App() {
 	return (
@@ -23,6 +24,7 @@ function App() {
 						<Route path='/product/:id' element={<ProductScreen />} />
 						<Route path='/cart' element={<CartScreen />} />
 						<Route path='/login' element={<LoginScreen />} />
+						<Route path='/registration' element={<RegistrationScreen />} />
 					</Routes>
 				</main>
 				<Footer />
--- client/src/redux/actions/userActions.js
+++ client/src/redux/actions/userActions.js
@@ -32,3 +32,30 @@ export const logout = () => (dispatch) => {
 	localStorage.removeItem('userInfo');
 	dispatch(userLogout());
 };
+
+export const register = (name, email, password) => async (dispatch) => {
+	dispatch(setLoading(true));
+	try {
+		const config = {
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		};
+
+		//if API successful, then data object is returned
+		const { data } = await axios.post('/api/users/register', { email, password, name }, config);
+		//Successful data object is dispatched and received as payload in slice, reducer userLogin
+		dispatch(userLogin(data));
+		localStorage.setItem('userInfo', JSON.stringify(data));
+	} catch (error) {
+		dispatch(
+			setError(
+				error.response && error.response.data
+					? error.response.data.message
+					: error.message
+					? error.message
+					: 'An unexpected error has occured. Please try again later.'
+			)
+		);
+	}
+};
