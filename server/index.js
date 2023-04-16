16b802e625591eb4764667ae7e402887a1e445f9
Evan Vargas <evanvargas@gmail.com>
2023-04-16 11:44:14 -0500

    Security Middleware

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import rateLimit from 'express-rate-limit';
+import helmet from 'helmet';
 import dotenv from 'dotenv';
 import connectToDatabase from './database.js';
 import express from 'express';
@@ -7,18 +8,25 @@ import express from 'express';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
+import mongoSanitize from 'express-mongo-sanitize';
+import xss from 'xss-clean';
+import hpp from 'hpp';
 
 dotenv.config();
 connectToDatabase();
 const app = express();
 const limiter = rateLimit({
 	max: 150,
-    windowMs: 60 * 60 * 1000,
-    message: 'Too many requests from this IP. Please try again in an hour!'
+	windowMs: 60 * 60 * 1000,
+	message: 'Too many requests from this IP. Please try again in an hour!',
 });
 //Global Middleware
+app.use(mongoSanitize());
+app.use(xss());
+app.use('/api', limiter);
+app.use(helmet());
+app.use(hpp());
 app.use(express.json());
-app.use('/api', limiter)
 const port = process.env.PORT || 5000;
 
 //Initializing routes and endpoints
